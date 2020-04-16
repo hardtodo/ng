a3081dd98e8168c76fe2ac74dd7b67a4bf01b3a9
YH <928895139@qq.com>
2020-04-16 15:06:29 +0800

    2020/4/16-15.26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,73 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'td';
+
+  shouldShow=false;
+
+  score =70;
+color:string;
+fontSize:string;
+
+  onToggle()
+  {
+    this.shouldShow=!this.shouldShow;
+    return false;
+  }
+
+  grade()
+  {
+    return Math.floor(this.score/10);
+  }
+
+  onInput(evt:Event)
+  {
+    const inputEle=<HTMLInputElement>evt.target;
+    this.score=Number(inputEle.value);
+  }
+
+  apply(color:string,fontSize:string)
+  {
+    this.color=color;
+    this.fontSize=fontSize;
+  }
+
+  isBordred=true;
+
+  toggleBorder()
+  {
+    this.isBordred=!this.isBordred;
+  }
+
+  classObj={
+    bordred:this.isBordred,blue:true,round:true
+  }
+
+  cities=['杭州','上海','温州'];
+
+  people=[];
+  peopleByCity=[];
+  constructor(){
+    this.people = [ 
+      { name: 'Anderson', age: 35, city: 'Sao Paulo' }, 
+      { name: 'John', age: 12, city: 'Miami' }, 
+      { name: 'Peter', age: 22, city: 'New York' } 
+      ];
+
+      this.peopleByCity = [ 
+        { city: 'Miami', 
+        people: [ 
+          { name: 'John', age: 12 }, 
+          { name: 'Angel', age: 22 } 
+          ] 
+        }, 
+        { 
+        city: 'Sao Paulo', 
+        people: [ 
+          { name: 'Anderson', age: 35 }, 
+          { name: 'Felipe', age: 36 } 
+          ] 
+        } 
+      ]; 
+
+  }
 }
